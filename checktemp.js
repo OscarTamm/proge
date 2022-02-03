c3be5848e88c545b27c3a191ae46931a26ea8cc6
Oscar Tammemäe <95708059+OscarTamm@users.noreply.github.com>
2022-02-03 12:59:26 +0200

    Update and rename countCode to checktemp.js

--- checktemp.js
+++ checktemp.js
@@ -17,13 +17,3 @@ function checktemp(temp){
 checktemp(5)
 
 
-function countCode(letter){
-    let count = 0;
-    for(let i = 0; i<DataCue.length;i++){
-        if(data[i][0]==letter){
-            count++;
-        }
-    }
-    console.log(count);
-    return count;
-}    
